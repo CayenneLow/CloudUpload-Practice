f444003d9da2db1eaf54f1ab83877c0ab8150d34
Khye Ean <lowkhyeean@gmail.com>
2018-10-26 16:07:42 +1100

    centered webpage

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+Bootstrap(app)
 db = SQLAlchemy(app)
 from routes import *
 
